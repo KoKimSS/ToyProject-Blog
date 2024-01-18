10417e5c3248628f5aa9fd280187064b1fa826a1
KoKimss <seungsu8848@ajou.ac.kr>
2024-01-18 20:05:03 +0900

    feat : application.yml active 변경

--- blogWorld/src/test/java/ToyProject/blogWorld/BlogControllerWorldApplicationTests.java
+++ blogWorld/src/test/java/ToyProject/blogWorld/BlogControllerWorldApplicationTests.java
@@ -3,7 +3,7 @@ package ToyProject.blogWorld;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BlogControllerWorldApplicationTests {
 
 	@Test
